4a11bcaf5727335d22ac3718dfac4803f9a38802
Italo Fernandes <italogsfernandes@gmail.com>
2017-05-06 09:53:43 -0300

    interface bluetooth

--- IDBracelet/app/src/main/java/ufu/biolab/italo/idbracelet/MainActivity.java
+++ IDBracelet/app/src/main/java/ufu/biolab/italo/idbracelet/MainActivity.java
@@ -1,28 +1,42 @@
 package ufu.biolab.italo.idbracelet;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    /*Button b1,b2,b3,b4;
+    private BluetoothAdapter bt_adapter;
+    private Set<BluetoothDevice> pairedDevices;
+    ListView lv_devices;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //b1 = (Button) findViewById(R.id.btn1);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Funcao a ser escrita, estou com preguiça agr", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
